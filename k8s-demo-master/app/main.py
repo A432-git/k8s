95ea82120acf3b5c7322f724ccf2766ce82b4c19
A432-git <62784581+A432-git@users.noreply.github.com>
2021-10-21 21:15:41 +0800

    add k8s  demo V2

--- k8s-demo-master/app/main.py
+++ k8s-demo-master/app/main.py
@@ -1,9 +1,10 @@
 from flask import Flask
+import socket
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello world V1!"
+    return f"Hello world V2 {socket.gethostname()}!"
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
